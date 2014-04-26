a5b19c0413dd0ba18be130be162f5118ff99d43e
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2014-04-26 20:11:11 +0900

    スタイルファイルのインストーラを変更
    - インストールに関わる情報を出力するように変更
    - スタイルファイルが正しくインストールされたか，インストール後にチェックする機能を追加
    - README.md にインストーラに関する記述を追加

--- install.sh
+++ install.sh
@@ -1,20 +1,68 @@
 #!/bin/bash
 
-exists_sty () {
-    kpsewhich "$1" > /dev/null
+echo_with_dots () {
+    str=$1
+    for (( i=${#1}; i<50; i++ )); do
+        str="$str."
+    done
+    echo -n $str
 }
+
+## コマンドの存在確認を行い，存在しなければエラーで終了
+check_command () {
+    echo_with_dots "Checking $1 "
+    if type "$1" >/dev/null; then
+        echo " ok";
+    else
+        echo " not found"
+        echo "** ERROR ** \`$1' command is not found. Install it!"
+        exit 1
+    fi
+}
+
+## スタイルファイルの存在確認を行う
+check_sty () {
+    echo_with_dots "Checking $1 "
+    path=`kpsewhich "$1" 2>/dev/null`
+    if [ -e "$path" ]; then
+        echo " $path"
+        return 0
+    else
+        echo " not found"
+        return 1
+    fi
+}
+
+## スタイルファイルが正しくインストールされているか確認し，
+## うまくいってなければエラーで終了
+check_sty_was_installed () {
+    if ! check_sty "$1"; then
+        echo "** ERROR ** Sorry! Failed to install \`$1'"
+        exit 1
+    fi
+}
+
+## インストールに必要なコマンドの存在確認
+check_command wget
+check_command bunzip2
+check_command git
+check_command kpsewhich
+check_command mktexlsr
+
+## スタイルファイルのインストール先を調べる
+echo_with_dots "Searching sty dir "
+
+TEXDIR=`kpsewhich amsmath.sty | sed 's|^\(/.*/tex\)/latex/.*$|\1|'`
+if [ "$TEXDIR" != "" ]; then
+    echo " $TEXDIR"
+else
+    echo " not found"
+    exit 1
+fi
 
 ## スーパユーザで実行していない場合はエラー
 if [ "$USER" != "root" ]; then
     echo "** ERROR ** Execute by root (use \`su' or \`sudo')"
-    exit 1
-fi
-
-## スタイルファイルのインストール先を調べる
-TEXDIR=`kpsewhich amsmath.sty | sed 's|^\(/.*/tex\)/latex/.*$|\1|'`
-
-if [ "$TEXDIR" = "" ]; then
-    echo "** ERROR ** Not found directory for .sty files"
     exit 1
 fi
 
@@ -33,11 +81,13 @@ cd "$WORKDIR"       # ワーキングディレクトリに移動して作業
 ##   listings.sty はデフォルトでも入っていますが，jlisting.sty は入っていないので，
 ##   インストールします．
 ##
-if ! exists_sty 'jlisting.sty'; then
+if ! check_sty 'jlisting.sty'; then
     wget 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmytexpert%2F26068%2Fjlisting.sty.bz2' -O jlisting.sty.bz2
     bunzip2 jlisting.sty.bz2
     mkdir -p "$TEXDIR/platex/jlisting"
     mv jlisting.sty "$TEXDIR/platex/jlisting"
+
+    check_sty_was_installed 'jlisting.sty' # 正しくインストールされたか確認
 fi
 
 ##
@@ -45,13 +95,15 @@ fi
 ##
 ##   Beamer で作ったスライドを dvipdfmx で PDF に変換すると，PDF の目次が文字化けする
 ##   ことがあります．プリアンブルで読み込んでおくと，PDF の目次が文字化けを防ぐことができ
-##   ます．PDF 自体は pxajahyper の有無にかかわらず，文字化けしないので問題ありませんが，
-##   入れておいて損はないでしょう．
+##   ます．PDF 自体は pxjahyper.sty の有無にかかわらず，文字化けしないので，大きな問題は
+##   ありませんが，入れておいて損はないでしょう．
 ##
-if ! exists_sty 'pxjahyper.sty'; then
+if ! check_sty 'pxjahyper.sty'; then
     git clone 'https://github.com/zr-tex8r/PXjahyper'
     mkdir -p "$TEXDIR/platex/pxjahyper"
     mv PXjahyper/pxjahyper.sty "$TEXDIR/platex/pxjahyper"
+
+    check_sty_was_installed 'pxjahyper.sty' # 正しくインストールされたか確認
 fi
 
 mktexlsr # TeX 関連ファイルのインデックスを更新
